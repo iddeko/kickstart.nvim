cbcf9b5c367bf247e92b753e2bc7b40d976862a6
Arthur-Aillet <arthur.aillet@epitech.eu>
2025-03-18 19:59:13 +0900

    feat: file explorer with auto open and auto close for maximum lazyness

--- init.lua
+++ init.lua
@@ -83,7 +83,6 @@ I hope you enjoy your Neovim journey,
 
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -272,10 +271,21 @@ require('lazy').setup({
 
   {
     'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
+    event = 'InsertEnter',
+    config = true,
     -- use opts = {} for passing setup options
     -- this is equivalent to setup({}) function
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    lazy = false,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('nvim-tree').setup {}
+    end,
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
@@ -1042,3 +1052,50 @@ vim.api.nvim_set_keymap('', '<Up>', '<Nop>', { noremap = true })
 vim.api.nvim_set_keymap('', '<Down>', '<Nop>', { noremap = true })
 vim.api.nvim_set_keymap('', '<Left>', '<Nop>', { noremap = true })
 vim.api.nvim_set_keymap('', '<Right>', '<Nop>', { noremap = true })
+
+local function open_nvim_tree()
+  -- open the tree
+  require('nvim-tree.api').tree.toggle {
+    focus = false,
+  }
+end
+
+local function tab_win_closed(winnr)
+  local api = require 'nvim-tree.api'
+  local tabnr = vim.api.nvim_win_get_tabpage(winnr)
+  local bufnr = vim.api.nvim_win_get_buf(winnr)
+  local buf_info = vim.fn.getbufinfo(bufnr)[1]
+  local tab_wins = vim.tbl_filter(function(w)
+    return w ~= winnr
+  end, vim.api.nvim_tabpage_list_wins(tabnr))
+  local tab_bufs = vim.tbl_map(vim.api.nvim_win_get_buf, tab_wins)
+  if buf_info.name:match '.*NvimTree_%d*$' then -- close buffer was nvim tree
+    -- Close all nvim tree on :q
+    if not vim.tbl_isempty(tab_bufs) then -- and was not the last window (not closed automatically by code below)
+      api.tree.close()
+    end
+  else -- else closed buffer was normal buffer
+    if #tab_bufs == 1 then -- if there is only 1 buffer left in the tab
+      local last_buf_info = vim.fn.getbufinfo(tab_bufs[1])[1]
+      if last_buf_info.name:match '.*NvimTree_%d*$' then -- and that buffer is nvim tree
+        vim.schedule(function()
+          if #vim.api.nvim_list_wins() == 1 then -- if its the last buffer in vim
+            vim.cmd 'quit' -- then close all of vim
+          else -- else there are more tabs open
+            vim.api.nvim_win_close(tab_wins[1], true) -- then close only the tab
+          end
+        end)
+      end
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd('WinClosed', {
+  callback = function()
+    local winnr = tonumber(vim.fn.expand '<amatch>')
+    vim.schedule_wrap(tab_win_closed(winnr))
+  end,
+  nested = true,
+})
+
+vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
